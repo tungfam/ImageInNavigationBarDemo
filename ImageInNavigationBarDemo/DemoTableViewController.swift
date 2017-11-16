13555dc5286e01ba28c86cde0db022f111a5e8ab
Tung Fam <tung@pixpie.co>
2017-11-17 01:28:18 +0200

    Add comments to properties

--- ImageInNavigationBarDemo/DemoTableViewController.swift
+++ ImageInNavigationBarDemo/DemoTableViewController.swift
@@ -12,17 +12,23 @@ class DemoTableViewController: UITableViewController {
 
     let imageView = UIImageView(image: UIImage(named: "image_name"))
 
+    /// WARNING: Change these constants according to your project's design
     private struct Const {
+        /// Image height/width for Large NavBar state
         static let ImageSizeForLargeState: CGFloat = 40
-        static let ImageRightSpaceForLargeState: CGFloat = 16
+        /// Spacing from right anchor of safe area to right anchor of Image
+        static let ImageRightSpace: CGFloat = 16
+        /// Spacing from bottom anchor of NavBar to bottom anchor of Image for Large NavBar state
         static let ImageBottomSpaceForLargeState: CGFloat = 12
+        /// Spacing from bottom anchor of NavBar to bottom anchor of Image for Small NavBar state
         static let ImageBottomSpaceForSmallState: CGFloat = 6
-        static let SmallImageFactor: CGFloat = 0.8 // Size for small icon is 32 = 40 * 0.8
+        /// Factor used to multiply your ImageSizeForLargeState to get the Image Size for Small NavBar State. For example: Image size for Large NavBar is 40, Image size for Small NavBar is 32, then value of SmallImageFactor = 32/40 = 0.8
+        static let SmallImageFactor: CGFloat = 0.8
 
+        /// Height of NavBar for Small state. Usually it's just 44
         static let NavBarHeightSmallState: CGFloat = 44
+        /// Height of NavBar for Large state. Usually it's just 96.5 but if you have a custom font for the title, please make sure to edit this value since it changes the height for Large state of NavBar
         static let NavBarHeightLargeState: CGFloat = 96.5
-
-        static let SummitHeaderNibName = "SummaryHeaderView"
     }
 
     override func viewDidLoad() {
@@ -34,22 +40,19 @@ class DemoTableViewController: UITableViewController {
 
         guard let navigationBar = self.navigationController?.navigationBar else { return }
 
+        // Initial setup for image for Large NavBar state since the the screen always has Large NavBar once it gets opened
         navigationBar.addSubview(imageView)
-
+        imageView.layer.cornerRadius = 20.0
+        imageView.clipsToBounds = true
         imageView.translatesAutoresizingMaskIntoConstraints = false
-
         NSLayoutConstraint.activate([
             imageView.rightAnchor.constraint(equalTo: navigationBar.rightAnchor,
-                                                              constant: -Const.ImageRightSpaceForLargeState),
+                                             constant: -Const.ImageRightSpace),
             imageView.bottomAnchor.constraint(equalTo: navigationBar.bottomAnchor,
-                                                               constant: -Const.ImageBottomSpaceForLargeState),
+                                              constant: -Const.ImageBottomSpaceForLargeState),
             imageView.heightAnchor.constraint(equalToConstant: Const.ImageSizeForLargeState),
             imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor)
             ])
-
-
-        imageView.layer.cornerRadius = 20.0
-        imageView.clipsToBounds = true
     }
 
     // MARK: - Table view data source
@@ -87,14 +90,17 @@ class DemoTableViewController: UITableViewController {
         let maxYTranslation = Const.ImageBottomSpaceForLargeState - Const.ImageBottomSpaceForSmallState + sizeDiff
 
         if height <= Const.NavBarHeightSmallState {
+            // If NavBar height matches or smaller than Small state we tranform image to small state
             imageView.transform = CGAffineTransform.identity
                 .scaledBy(x: Const.SmallImageFactor, y: Const.SmallImageFactor)
                 .translatedBy(x: sizeDiff, y: maxYTranslation)
         } else if height >= Const.NavBarHeightLargeState {
+            // If NavBar height matches or bigger than Large state we tranform image to large state (basically to normal size)
             imageView.transform = CGAffineTransform.identity
                 .scaledBy(x: 1.0, y: 1.0)
                 .translatedBy(x: 0.0, y: 0.0)
         } else {
+            // If NavBar is in between of Small and Large states we tranform image and move bottom and right spacings proportionally to the size of NavBar. This way it creates a smooth animation for image resizing
             let delta = height - Const.NavBarHeightSmallState
             let coeff = delta / (Const.NavBarHeightLargeState - Const.NavBarHeightSmallState)
             let sizeAddendumFactor = coeff * (1.0 - Const.SmallImageFactor)
